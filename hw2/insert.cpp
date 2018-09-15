db69d365b9e1cc91da0bf0af146d1c1bd6ff20d9
Daniel Parker <daniel12997@gmail.com>
2018-09-15 00:23:58 -0400

    Changed some insertion sort stuff

--- hw2/insert.cpp
+++ hw2/insert.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
-#include <unistd.h>
 #include <ctime>
 #include <vector>
+#include <stdint.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
 
 using namespace std;
 
-void insertionSort(vector<u_int16_t>);
+void insertionSort(vector<uint16_t> &A);
 
 int main() {
-	int runningTimes[11] = {100, 500, 1000, 5000, 10000, 50000, 100000, 10, 10, 10, 10};
+	int runningTimes[11] = {100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000};
 	printf("Insertion Sort\n");
 	for(int i = 0; i < 11; ++i) {
-
-		vector<u_int16_t> A(runningTimes[i]);
-		for(int j = 0; j <=runningTimes[i]; ++j) {
-			srand(clock());
-			A.push_back(rand());
+		srand(clock());
+		vector<uint16_t> A(runningTimes[i]);
+		for(int j = 0; j < runningTimes[i]; ++j) {
+			A[j] = rand();
 		}
-
 		int init = clock();
 		insertionSort(A);
 		float runtime = static_cast<float>(clock() - init) / static_cast<float>(CLOCKS_PER_SEC);
 		printf("%i,%f\n", runningTimes[i], runtime);
+		for (int j = 1; j < A.size(); j++) {
+			//cout << A[j] << ", ";
+			if (A[j] < A[j - 1]) {
+				cout << "WRONG " << j;
+			}
+		}
 	}
 	string tmp;
 	getline(cin, tmp);
 }
 
-void insertionSort(vector<u_int16_t> A) {
-	for(int i = 1; i <= A.size(); ++i) {
+void insertionSort(vector<uint16_t> &A) {
+	for(int i = 1; i < A.size(); ++i) {
 		int key = A[i];
 		int j = i -1;
 		while(j >= 0 and A[j] > key) {
@@ -36,11 +44,5 @@ void insertionSort(vector<u_int16_t> A) {
 			j--;
 		}
 		A[j + 1] = key;
-	}
-
-	for(int i = 0; i <= A.size(); ++i) {
-		if(A[i] < A[i-1]) {
-			cout << "WRONG\n";
-		}
 	}
 }
