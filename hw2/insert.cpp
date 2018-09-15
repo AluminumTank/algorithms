64f8ef305a2807bbc1c61d1cf6a346458ff82a72
Daniel Parker <daniel12997@gmail.com>
2018-09-15 18:12:13 -0400

    Changed the timer in radix and insertion sort.

--- hw2/insert.cpp
+++ hw2/insert.cpp
@@ -5,13 +5,14 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <chrono>
 
 
 using namespace std;
 
 void insertionSort(vector<uint16_t> &A);
 
-/*int main() {
+int main() {
 	int runningTimes[11] = {100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000};
 	printf("Insertion Sort\n");
 	for(int i = 0; i < 11; ++i) {
@@ -20,10 +21,13 @@ void insertionSort(vector<uint16_t> &A);
 		for(int j = 0; j < runningTimes[i]; ++j) {
 			A[j] = rand();
 		}
-		int init = clock();
+		auto init = chrono::high_resolution_clock::now();
 		insertionSort(A);
-		float runtime = static_cast<float>(clock() - init) / static_cast<float>(CLOCKS_PER_SEC);
-		printf("%i,%f\n", runningTimes[i], runtime);
+		auto end = chrono::high_resolution_clock::now();
+		auto duration2 = end - init;
+		int sec2 = chrono::duration_cast<chrono::seconds>(duration2).count();
+		int nano2 = chrono::duration_cast<chrono::nanoseconds>(duration2).count() % 1000000000;
+		printf("%i, %i.%09i\n", runningTimes[i], sec2, nano2);
 		for (int j = 1; j < A.size(); j++) {
 			//cout << A[j] << ", ";
 			if (A[j] < A[j - 1]) {
@@ -33,7 +37,7 @@ void insertionSort(vector<uint16_t> &A);
 	}
 	string tmp;
 	getline(cin, tmp);
-}*/
+}
 
 void insertionSort(vector<uint16_t> &A) {
 	for(int i = 1; i < A.size(); ++i) {
--- hw2/radix.cpp
+++ hw2/radix.cpp
@@ -12,31 +12,46 @@
 using namespace std;
 
 void radixSort(uint16_t * A, uint16_t digit, int len);
+void quickSort(uint16_t arr[], int low, int high);
 
-int main() {
+
+/*int main() {
 	int runningTimes[11] = {100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000};
 	printf("Radix Sort\n");
 	for(int i = 0; i < 11; ++i) {
 		srand(clock());
 		uint16_t * A = new uint16_t[runningTimes[i]];
+		uint16_t * B = new uint16_t[runningTimes[i]];
 		for(int j = 0; j < runningTimes[i]; ++j) {
 			A[j] = rand();
+			B[j] = A[j];
 		}
-		int init = clock();
+		auto init = chrono::high_resolution_clock::now();
 		radixSort(A, 16, runningTimes[i]);
-		float runtime = static_cast<float>(clock() - init) / static_cast<float>(CLOCKS_PER_SEC);
-		printf("%i,%f\n", runningTimes[i], runtime);
+		auto end = chrono::high_resolution_clock::now();
+		auto duration1 = end - init;
+		int sec1 = chrono::duration_cast<chrono::seconds>(duration1).count();
+		int nano1 = chrono::duration_cast<chrono::nanoseconds>(duration1).count() % 1000000000;
+
+		printf("%i, %i.%09i\n", runningTimes[i], sec1, nano1);
 		for (int j = 1; j < runningTimes[i]; j++) {
 			//cout << A[j] << ", ";
 			if (A[j] < A[j - 1]) {
-				cout << "WRONG " << j;
+				cout << "WRONGA " << j;
+			}
+		}
+		for (int j = 1; j < runningTimes[i]; j++) {
+			//cout << A[j] << ", ";
+			if (B[j] < B[j - 1]) {
+				cout << "WRONGB " << j;
 			}
 		}
 		delete[] A;
+		delete[] B;
 	}
 	string tmp;
 	getline(cin, tmp);
-}
+}*/
 
 void radixSort(uint16_t * A, uint16_t digit, int len) {
 	if (len > 1 && digit > 0) {
