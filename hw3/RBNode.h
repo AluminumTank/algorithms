6e900b854e1b2d7af3702ba0f11c06f612245085
Daniel Parker <daniel12997@gmail.com>
2018-09-29 22:54:46 -0400

    change

--- hw3/RBNode.h
+++ hw3/RBNode.h
@@ -17,6 +17,10 @@ public:
 	RBNode() {}
 	RBNode(T newKey, int newColor);
 
+	RBNode<T> * getParent() override;
+	RBNode<T> * getRight() override;
+	RBNode<T> * getLeft() override;
+
 	int getColor();
 
 	void setColor(int newColor);
@@ -28,6 +32,24 @@ private:
 template <class T>
 RBNode<T>::RBNode(T newKey, int newColor) : BNode(newKey) {
 	color = newColor;
+}
+
+template<class T>
+RBNode<T>* RBNode<T>::getParent()
+{
+	return static_cast<RBNode<T> *>(BNode<T>::getParent());
+}
+
+template<class T>
+RBNode<T>* RBNode<T>::getRight()
+{
+	return static_cast<RBNode<T> *>(BNode<T>::getRight());
+}
+
+template<class T>
+RBNode<T>* RBNode<T>::getLeft()
+{
+	return static_cast<RBNode<T> *>(BNode<T>::getLeft());
 }
 
 template <class T>
